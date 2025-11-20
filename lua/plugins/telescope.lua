284d2c7bd1e001b970876c3d6053f8e64aa193a8
Prawirdani <prawirdani@gmail.com>
2025-11-20 17:33:22 +0700

    fzf-native

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,19 @@ return {
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+      'nvim-telescope/telescope-fzf-native.nvim',
+
+      -- `build` is used to run some command when the plugin is installed/updated.
+      -- This is only run then, not every time Neovim starts up.
+      build = 'make',
+
+      -- `cond` is a condition used to determine whether this plugin should be
+      -- installed and loaded.
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    },
     { 'nvim-telescope/telescope-ui-select.nvim' },
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
@@ -13,6 +26,8 @@ return {
       pickers = {
         find_files = {
           hidden = true,
+          -- needed to exclude some files & dirs from general search
+          -- when not included or specified in .gitignore
           find_command = {
             'rg',
             '--files',
@@ -35,16 +50,19 @@ return {
         },
       },
       defaults = {
+        -- configure to use ripgrep
         vimgrep_arguments = {
           'rg',
-          '--follow',
-          '--hidden',
-          '--no-heading',
-          '--with-filename',
-          '--line-number',
-          '--column',
-          '--smart-case',
+          '--follow', -- Follow symbolic links
+          '--hidden', -- Search for hidden files
+          '--no-heading', -- Don't group matches by each file
+          '--with-filename', -- Print the file path with the matched lines
+          '--line-number', -- Show line numbers
+          '--column', -- Show column numbers
+          '--smart-case', -- Smart case search
           '--no-ignore-vcs',
+
+          -- Exclude some patterns from search
           '--glob=!**/.git/*',
           '--glob=!**/.idea/*',
           '--glob=!**/__pycache__/*',
@@ -68,9 +86,11 @@ return {
       },
     }
 
-    -- Enable UI select extension
+    -- Enable Telescope extensions if they are installed
+    pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
 
+    -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
@@ -87,6 +107,8 @@ return {
       { desc = '[S]earch Recent Files ("." for repeat)' }
     )
 
+    -- It's also possible to pass additional configuration options.
+    --  See `:help telescope.builtin.live_grep()` for information about particular keys
     vim.keymap.set('n', '<leader>s/', function()
       builtin.live_grep {
         grep_open_files = true,
@@ -94,6 +116,7 @@ return {
       }
     end, { desc = '[S]earch [/] in Open Files' })
 
+    -- Shortcut for searching your Neovim configuration files
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = '[S]earch [N]eovim files' })
